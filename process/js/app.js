127bdb16358662044207812d65bb20469aa386fd
Deluxateer <deluxateer@gmail.com>
2018-01-30 21:38:12 -0500

    tested multiple audio event handling

--- process/js/app.js
+++ process/js/app.js
@@ -1,16 +1,23 @@
+
+
+
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
+  console.log(e.target);
   e.target.classList.remove('playing');
 }
 
 function playSound(e) {
-  const audio = document.querySelector('.track');
-  const key = document.querySelector('.target');
+  const item = e.target;
+  const audio = item.querySelector('.track');
+  const key = item.querySelector('.target');
 
-  key.classList.add('playing');
+  // item.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
 }
 
-document.querySelector('.target').addEventListener('transitionend', removeTransition);
-document.querySelector('.target').addEventListener('click', playSound);
+document.querySelectorAll('.brd-btn').forEach( track => {
+  // track.addEventListener('transitionend', removeTransition);
+  track.addEventListener('click', playSound);
+});
